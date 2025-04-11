4d3d6f70c54f020bf8409947376aca01bf15525c
raj1008-git <macof.rj@gmail.com>
2025-04-11 13:30:34 +0545

    commented the unnecessary codes

--- lib/features/call_listener/logic/call_channel.dart
+++ lib/features/call_listener/logic/call_channel.dart
@@ -1,16 +1,16 @@
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
-class CallChannel {
-  static const MethodChannel _channel = MethodChannel(
-    'com.example.employee/call',
-  );
+// class CallChannel {
+//   static const MethodChannel _channel = MethodChannel(
+//     'com.example.employee/call',
+//   );
 
-  static void setCallListener(Function(String) onNumberReceived) {
-    _channel.setMethodCallHandler((call) async {
-      if (call.method == "incomingCall") {
-        String number = call.arguments;
-        onNumberReceived(number);
-      }
-    });
-  }
-}
+//   static void setCallListener(Function(String) onNumberReceived) {
+//     _channel.setMethodCallHandler((call) async {
+//       if (call.method == "incomingCall") {
+//         String number = call.arguments;
+//         onNumberReceived(number);
+//       }
+//     });
+//   }
+// }
--- lib/features/call_listener/ui/call_display_screen.dart
+++ lib/features/call_listener/ui/call_display_screen.dart
@@ -1,37 +1,37 @@
-import 'package:flutter/material.dart';
-import '../logic/call_channel.dart';
+// import 'package:flutter/material.dart';
+// import '../logic/call_channel.dart';
 
-class CallDisplayScreen extends StatefulWidget {
-  const CallDisplayScreen({super.key});
+// class CallDisplayScreen extends StatefulWidget {
+//   const CallDisplayScreen({super.key});
 
-  @override
-  _CallDisplayScreenState createState() => _CallDisplayScreenState();
-}
+//   @override
+//   _CallDisplayScreenState createState() => _CallDisplayScreenState();
+// }
 
-class _CallDisplayScreenState extends State<CallDisplayScreen> {
-  String _callerNumber = 'Waiting for calls...';
+// class _CallDisplayScreenState extends State<CallDisplayScreen> {
+//   String _callerNumber = 'Waiting for calls...';
 
-  @override
-  void initState() {
-    super.initState();
-    CallChannel.setCallListener((number) {
-      setState(() {
-        _callerNumber = 'Incoming call from : $number';
-      });
-    });
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     CallChannel.setCallListener((number) {
+//       setState(() {
+//         _callerNumber = 'Incoming call from : $number';
+//       });
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Call Listener')),
-      body: Center(
-        child: Text(
-          _callerNumber,
-          style: TextStyle(fontSize: 20),
-          textAlign: TextAlign.center,
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('Call Listener')),
+//       body: Center(
+//         child: Text(
+//           _callerNumber,
+//           style: TextStyle(fontSize: 20),
+//           textAlign: TextAlign.center,
+//         ),
+//       ),
+//     );
+//   }
+// }
