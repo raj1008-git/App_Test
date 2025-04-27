601f3b5067a4b29ef4f81b7e2d5b109ac4035a6e
raj1008-git <macof.rj@gmail.com>
2025-04-27 13:04:53 +0545

    Added the custom sizes

---
+++ lib/core/constants/app_sizes.dart
@@ -0,0 +1,9 @@
+class AppSizes {
+  static const double spaceSmall = 8.0;
+  static const double spaceMedium = 16.0;
+  static const double spaceLarge = 32.0;
+
+  static const double textSmall = 12.0;
+  static const double textMedium = 16.0;
+  static const double textLarge = 20;
+}
