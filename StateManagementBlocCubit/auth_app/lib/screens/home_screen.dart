7bcfaa9bd96edc9633a831890e47d331c8abdc2e
raj1008-git <macof.rj@gmail.com>
2025-06-07 20:44:57 +0545

    Home screen implemented

---
+++ StateManagementBlocCubit/auth_app/lib/screens/home_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../cubit/auth_cubit.dart';
+import '../cubit/auth_state.dart';
+import '../cubit/profile_cubit.dart';
+import '../cubit/profile_state.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final authState = context.watch<AuthCubit>().state;
+    if (authState is! Authenticated) {
+      return const Scaffold(body: Center(child: Text('Not Logged in')));
+    }
+    final user = authState.user;
+    final profileCubit = context.read<ProfileCubit>();
+    profileCubit.loadUserProfile(user.id);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome ${user.name}'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.read<AuthCubit>().logout();
+              Navigator.pushReplacementNamed(context, '/login');
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: BlocBuilder(
+        builder: (context, state) {
+          if (state is ProfileLoaded) {
+            return Center(child: Text('Bio: ${state.bio}'));
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
